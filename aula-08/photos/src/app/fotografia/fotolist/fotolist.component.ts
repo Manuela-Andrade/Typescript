efbfb8e8f40f08957527aadb439daf321109a542
manuandrade-crf <rjmanuelaandrade@gmail.com>
2022-11-10 11:48:53 -0300

    Atualizando aula-08

--- aula-08/photos/src/app/fotografia/fotolist/fotolist.component.ts
+++ aula-08/photos/src/app/fotografia/fotolist/fotolist.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Foto } from './foto.model';
 
 @Component({
   selector: 'pho-fotolist',
@@ -7,39 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class FotolistComponent implements OnInit {
   
   //Array de objetos
-  imagens = [
-    {
-      img: "https://cdn.pixabay.com/photo/2022/10/24/14/48/forest-7543646_960_720.jpg",
-      titulo: "Floresta",
-      numero: 1
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2022/10/09/02/16/haunted-house-7508035_960_720.jpg",
-      titulo: "Casa assombrada",
-      numero: 2
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
-      titulo: "Lago",
-      numero: 3
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_960_720.jpg",
-      titulo: "Casa do Lago",
-      numero: 4
-    },
-    {
-      img: "https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_960_720.jpg",
-      titulo: "Ilha",
-      numero: 5
-    }
-  ];
-
-
-
-  constructor() { }
+  imagens: Foto[]=[];
+  //Injeção de dependencia é quando solicitamos uma ferramenta.
+  //Toda injeção de dependencia deve ser feita no () do constructor
+  //Toda ferramenta precisa de alguém para usa-la (ex: esse alguem é a variável http (manuela))
+  constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<Foto[]>('http://localhost:3000/imagem').subscribe(caixa => this.imagens =caixa);
   }
 
 }
--- aula-08/photos/src/app/shared/shared.module.ts
+++ aula-08/photos/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   exports: [
     CommonModule,
     BrowserModule,
+    HttpClientModule
   ]
 })
 export class SharedModule { }
